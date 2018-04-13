4e8e881129d578f302b6fc0fcd19dab575e71f39
Rens <feeling4design@gmail.com>
2018-04-13 17:08:01 +0200

    placeholder and default value mixed up
    Fix: "Save custom user meta" placeholder and default value mixed up, causing it to not save the setting when saving form the next time the builder page was reloaded

--- super-forms-register-login.php
+++ super-forms-register-login.php
@@ -11,7 +11,7 @@
  * Plugin Name: Super Forms - Register & Login
  * Plugin URI:  http://codecanyon.net/item/super-forms-drag-drop-form-builder/13979866
  * Description: Makes it possible to let users register and login from the front-end
- * Version:     1.5.2
+ * Version:     1.5.3
  * Author:      feeling4design
  * Author URI:  http://codecanyon.net/user/feeling4design
 */
@@ -37,7 +37,7 @@ if(!class_exists('SUPER_Register_Login')) :
          *
          *  @since      1.0.0
         */
-        public $version = '1.5.2';
+        public $version = '1.5.3';
 
 
         /**
@@ -913,7 +913,8 @@ if(!class_exists('SUPER_Register_Login')) :
                         'name' => __( 'Save custom user meta', 'super-forms' ),
                         'label' => __( 'Usefull for external plugins such as WooCommerce. Example: \'field_name|meta_key\' (each on a new line)', 'super-forms' ),
                         'type' => 'textarea',
-                        'placeholder' => SUPER_Settings::get_value( 0, 'register_login_user_meta', $settings['settings'], "field_name|meta_key\nbilling_first_name|billing_first_name\nbilling_last_name|billing_last_name\nbilling_company|billing_company\nbilling_address_1|billing_address_1\nbilling_address_2|billing_address_2\nbilling_city|billing_city\nbilling_postcode|billing_postcode\nbilling_country|billing_country\nbilling_state|billing_state\nbilling_phone|billing_phone\nbilling_email|billing_email\nshipping_first_name|shipping_first_name\nshipping_last_name|shipping_last_name\nshipping_company|shipping_company\nshipping_address_1|shipping_address_1\nshipping_address_2|shipping_address_2\nshipping_city|shipping_city\nshipping_postcode|shipping_postcode\nshipping_country|shipping_country\nshipping_state|shipping_state" ),
+                        'default' => SUPER_Settings::get_value( 0, 'register_login_user_meta', $settings['settings'], '' ),
+                        'placeholder' => "field_name|meta_key\nbilling_first_name|billing_first_name\nbilling_last_name|billing_last_name\nbilling_company|billing_company\nbilling_address_1|billing_address_1\nbilling_address_2|billing_address_2\nbilling_city|billing_city\nbilling_postcode|billing_postcode\nbilling_country|billing_country\nbilling_state|billing_state\nbilling_phone|billing_phone\nbilling_email|billing_email\nshipping_first_name|shipping_first_name\nshipping_last_name|shipping_last_name\nshipping_company|shipping_company\nshipping_address_1|shipping_address_1\nshipping_address_2|shipping_address_2\nshipping_city|shipping_city\nshipping_postcode|shipping_postcode\nshipping_country|shipping_country\nshipping_state|shipping_state",
                         'filter' => true,
                         'parent' => 'register_login_action',
                         'filter_value' => 'register',
